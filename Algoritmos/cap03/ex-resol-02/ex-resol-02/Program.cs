609044c36d316e21eeb6b51d137f880e41d8e6e9
VitorDLeo <vitordileo003@gmail.com>
2022-05-22 16:58:21 -0300

    Ex resolvido 2

---
+++ Algoritmos/cap03/ex-resol-02/ex-resol-02/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace ex_resol_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double h, b, AREA, PERIMETRO, DIAGONAL;
+
+            Console.Write("Digite o valor da base(b): ");
+            b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Digite o valor da Altura(h): ");
+            h = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            AREA = h * b;
+            PERIMETRO = 2.00 * (b + h);
+            DIAGONAL = Math.Sqrt(Math.Pow(h, 2.0) + Math.Pow(b, 2.0));
+
+            Console.WriteLine("AREA = " + AREA.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PERIMETRO = " + PERIMETRO.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("DIAGONAL = " + DIAGONAL.ToString("F4", CultureInfo.InvariantCulture));
+        }
+    }
+}
