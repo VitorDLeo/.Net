39e8da43845ee6d4f6f84d6ba97ed97229ee31f9
VitorDLeo <vitordileo003@gmail.com>
2022-05-22 01:25:46 -0300

    XXXXXXXXXXXXX

---
+++ Algoritmos/cap03/URI-ex-1016/URI-ex-1016/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace uri1016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int distancia, tempo;
+
+            distancia = int.Parse(Console.ReadLine());
+
+            tempo = distancia * 2;
+
+            Console.WriteLine(tempo + " minutos");
+        }
+    }
+}
---
+++ Algoritmos/cap03/URI-ex-1017/URI-ex-1017/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace URI_ex_1017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hora, velocidadeMedia;
+            double litrosGasto;
+
+            hora = int.Parse(Console.ReadLine());
+            velocidadeMedia = int.Parse(Console.ReadLine());
+
+            litrosGasto = (hora * velocidadeMedia) / 12.0;
+
+            Console.WriteLine(litrosGasto.ToString("F3", CultureInfo.InvariantCulture));
+        }
+    }
+}
