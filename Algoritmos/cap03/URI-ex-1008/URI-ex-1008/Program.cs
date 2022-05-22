17afacf47ca5ed32143039d93a9db58e5024b8e1
VitorDLeo <vitordileo003@gmail.com>
2022-05-22 00:51:24 -0300

    XXXXXXXXXXXXX

--- Algoritmos/cap03/URI-ex-1008/URI-ex-1008/Program.cs
+++ Algoritmos/cap03/URI-ex-1008/URI-ex-1008/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace URI_ex_1008
 {
@@ -6,7 +7,17 @@ namespace URI_ex_1008
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int NUMBER, horasTrabalhadas;
+            double valorHora, SALARY;
+
+            NUMBER = int.Parse(Console.ReadLine());
+            horasTrabalhadas = int.Parse(Console.ReadLine());
+            valorHora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            SALARY = horasTrabalhadas * valorHora;
+
+            Console.WriteLine("NUMBER = " + NUMBER);
+            Console.WriteLine("SALARY = U$ " + SALARY.ToString("F2", CultureInfo.InvariantCulture));
         }
     }
 }
---
+++ Algoritmos/cap03/URI-ex-1010/URI-ex-1010/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace URI_ex_1010
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pc1, pc2, qt1, qt2;
+            double valorPc1, valorPc2, total;
+
+            string [] vet = Console.ReadLine().Split(' ');
+            pc1 = int.Parse(vet[0]);
+            qt1 = int.Parse(vet[1]);
+            valorPc1 = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+            vet = Console.ReadLine().Split(' ');
+            pc2 = int.Parse(vet[0]);
+            qt2 = int.Parse(vet[1]);
+            valorPc2 = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+            total = qt1 * valorPc1 + qt2 * valorPc2;
+
+            Console.WriteLine("VALOR A PAGAR: R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
