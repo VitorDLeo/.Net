f9d663763c33921ef974b61bb71a90ec4146b7cc
VitorDLeo <vitordileo003@gmail.com>
2022-05-23 19:01:29 -0300

    xxxxxxxxxxxxxxx

---
+++ Algoritmos/cap04/ex-res-02/ex-res-02/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+namespace FuncoesMatematicas_EquacoesQuadraticas
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.Write("Digite o valor de a: ");
+                float a = float.Parse(Console.ReadLine());
+
+                Console.Write("Digite o valor de b: ");
+                float b = float.Parse(Console.ReadLine());
+
+                Console.Write("Digite o valor de c: ");
+                float c = float.Parse(Console.ReadLine());
+
+                Console.WriteLine("----------------------------");
+                double delta = Math.Pow(b, 2.0) - (4 * a * c);
+
+                if(delta > 0)
+                {
+                    Console.WriteLine("Possivel de calcular, existem duas raizes");
+                } else if(delta < 0)
+                {
+                    Console.WriteLine("Impossivel de calcular");
+                } else
+                {
+                    Console.WriteLine("Delta negativo, so existe uma raiz");
+                }
+
+                Console.WriteLine("O valor de delta e = " + delta);
+
+                double x1 = (-b + Math.Sqrt(delta)) / (2.0 * a);
+                double x2 = (-b - Math.Sqrt(delta)) / (2.0 * a);
+
+                Console.WriteLine("------------------------------");
+                Console.WriteLine("o valor de x1 = " + x1);
+                Console.WriteLine("o valor de x2 = " + x2);
+            }
+        }
+    }
+    
+
