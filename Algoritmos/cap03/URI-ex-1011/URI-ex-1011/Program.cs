3a8dac875e5c9a76838c89bb36dc01474581b315
VitorDLeo <vitordileo003@gmail.com>
2022-05-23 01:11:29 -0300

    xxxxxxxxx

---
+++ Algoritmos/cap03/URI-ex-1011/URI-ex-1011/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace URI_ex_1011
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double R, VOLUME;
+            double pi = 3.14159;
+
+            R = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            VOLUME = (double) (4.0 / 3.0) * pi * Math.Pow(R, 3.0);
+
+            Console.WriteLine("VOLUME = " + VOLUME.ToString("F3", CultureInfo.InvariantCulture));
+        }
+    }
+}
