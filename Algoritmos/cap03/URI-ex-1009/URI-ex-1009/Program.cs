5d40912176b849aee7ffb2e099e1a1379ee07718
VitorDLeo <vitordileo003@gmail.com>
2022-05-23 00:54:30 -0300

    xxxxxxxxx

---
+++ Algoritmos/cap03/URI-ex-1009/URI-ex-1009/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace URI_ex_1009
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            double salario, totalVendas, TOTAL;
+
+            nome = Console.ReadLine();
+            salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            totalVendas = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            TOTAL = totalVendas * (15.0 / 100.0) + salario;
+
+            Console.WriteLine("TOTAL = R$ " + TOTAL.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
--- Algoritmos/cap03/ex-resol-03/ex-resol-03/Program.cs
+++ Algoritmos/cap03/ex-resol-03/ex-resol-03/Program.cs
@@ -9,9 +9,10 @@ namespace ex_resol_03
         {
             double idadeA, idadeB, media;
             string nomeA, nomeB;
+            string[] valores;
 
             Console.Write("Nome e idade A: ");
-            string[] valores = Console.ReadLine().Split(' ');
+            valores = Console.ReadLine().Split(' ');
             nomeA = valores[0];
             idadeA = double.Parse(valores[1]);
 
@@ -20,7 +21,7 @@ namespace ex_resol_03
             nomeB = valores[0];
             idadeB = double.Parse(valores[1]);
 
-            media = (idadeA + idadeB) / 2.00;
+            media = (double) (idadeA + idadeB) / 2.00;
 
             Console.WriteLine("A idade media de " + nomeA + " e " + nomeB + " e de " + media.ToString("F1", CultureInfo.InvariantCulture) + " anos!");
         }
