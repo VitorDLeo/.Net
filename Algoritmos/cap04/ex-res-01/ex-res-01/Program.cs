fe7966a134d06359e4c6ffbbc2e76845faa7b723
VitorDLeo <vitordileo003@gmail.com>
2022-05-23 18:51:09 -0300

    xxxxxxxxxxxxxxx

---
+++ Algoritmos/cap04/ex-res-01/ex-res-01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace ex_res_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1, n2, notaFinal;
+
+            n1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            notaFinal = (n1 + n2) / 2.00;
+
+            if (notaFinal >= 60.0){
+                Console.WriteLine("NOTA FINAL = " + notaFinal.ToString("F1", CultureInfo.InvariantCulture));
+                Console.WriteLine("APROVADO!");
+            } else {
+                Console.WriteLine("NOTA FINAL = " + notaFinal.ToString("F1", CultureInfo.InvariantCulture));
+                Console.WriteLine("REPROVADO!");
+            }
+        }
+    }
+}
