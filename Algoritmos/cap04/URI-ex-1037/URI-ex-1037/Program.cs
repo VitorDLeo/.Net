1379543305bc8a4866310fc8ac3a777bd09db061
VitorDLeo <vitordileo003@gmail.com>
2022-05-24 15:49:57 -0300

    XXXXXXXXXXXXX

---
+++ Algoritmos/cap04/URI-ex-1037/URI-ex-1037/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace URI_ex_1037
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor;
+
+            valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            if (valor < 0.00 || valor > 100.00)
+            {
+                Console.WriteLine("Fora de intervalo");
+            } else if (valor <= 25.00)
+            {
+                Console.WriteLine("Intervalo [0,25]");
+            }
+            else if (valor <= 50)
+            {
+                Console.WriteLine("Intervalo (25,50]");
+            } else if (valor <= 75.00)
+            {
+                Console.WriteLine("Intervalo (50,75]");
+            } else if (valor <= 100.00)
+            {
+                Console.WriteLine("Intervalo (75,100]");
+            } 
+        }
+    }
+}
