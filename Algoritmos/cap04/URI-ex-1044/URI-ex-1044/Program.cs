37dc5a6b8d10cc8c7a1a8306732acc7a55c5f735
VitorDLeo <vitordileo003@gmail.com>
2022-05-24 00:25:20 -0300

    xxxxxxxxxxxxxxx

---
+++ Algoritmos/cap04/URI-ex-1044/URI-ex-1044/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace URI_ex_1044
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vet;
+            int A, B;
+
+            vet = Console.ReadLine().Split(' ');
+            A = int.Parse(vet[0]);
+            B = int.Parse(vet[1]);
+
+            if(A % B == 0 || B % A == 0)
+            {
+                Console.WriteLine("Sao Multiplos");
+            } else
+            {
+                Console.WriteLine("Nao sao Multiplos");
+            }
+        }
+    }
+}
