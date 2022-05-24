363e91ca286f18f94a7ea907ad7bf7bb70c5bbb3
VitorDLeo <vitordileo003@gmail.com>
2022-05-23 23:53:15 -0300

    xxxxxxxxxxxxxxx

---
+++ Algoritmos/cap04/URI-ex-1035/URI-ex-1035/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace URI_ex_1035
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, C, D;
+            string[] vet;
+
+            vet = Console.ReadLine().Split(' ');
+
+            A = int.Parse(vet[0]);
+            B = int.Parse(vet[1]);
+            C = int.Parse(vet[2]);
+            D = int.Parse(vet[3]);
+
+            if((B > C && D > A) && (C + D > A + B))
+            {
+                Console.WriteLine("Valores aceitos");
+            } else
+            {
+                Console.WriteLine("Valores nao aceitos");
+            }
+        }
+    }
+}
