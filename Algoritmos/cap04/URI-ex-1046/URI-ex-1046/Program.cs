85a492fad3a9a8b4e1cd8724e3fd2506de47eb06
VitorDLeo <vitordileo003@gmail.com>
2022-05-24 12:09:22 -0300

    XXXXXXXXXXXXX

---
+++ Algoritmos/cap04/URI-ex-1046/URI-ex-1046/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace URI_ex_1046
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int horaInicial, horaFinal, duracao;
+            string[] vet;
+
+            vet = Console.ReadLine().Split(' ');
+            horaInicial = int.Parse(vet[0]);
+            horaFinal = int.Parse(vet[1]);
+
+            if(horaInicial < horaFinal)
+            {
+                duracao = horaFinal - horaInicial;
+            } else
+            {
+                duracao = 24 - horaInicial + horaFinal;
+            }
+
+            Console.WriteLine("O JOGO DUROU " + duracao + " HORA(S)");
+        }
+    }
+}
