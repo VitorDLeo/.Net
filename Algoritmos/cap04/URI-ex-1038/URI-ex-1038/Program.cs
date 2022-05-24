50914947aacf730db509e6ce149fe0ba1a234cde
VitorDLeo <vitordileo003@gmail.com>
2022-05-24 00:10:33 -0300

    xxxxxxxxxxxxxxx

--- Algoritmos/cap04/URI-ex-1038/URI-ex-1038/Program.cs
+++ Algoritmos/cap04/URI-ex-1038/URI-ex-1038/Program.cs
@@ -1,25 +1,40 @@
 ﻿using System;
 using System.Globalization;
 
-namespace URI_ex_1038
+namespace uri1038
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int cod, qtd;
+
+            string[] valores = Console.ReadLine().Split(' ');
+            int codigo = int.Parse(valores[0]);
+            int quantidade = int.Parse(valores[1]);
+
             double total;
-            string[] vet;
-
-            vet = Console.ReadLine().Split(' ');
-            cod = int.Parse(vet[0]);
-            qtd = int.Parse(vet[1]);
-
-            switch (cod)
+            if (codigo == 1)
             {
-
+                total = quantidade * 4.0;
             }
-            
+            else if (codigo == 2)
+            {
+                total = quantidade * 4.5;
+            }
+            else if (codigo == 3)
+            {
+                total = quantidade * 5.0;
+            }
+            else if (codigo == 4)
+            {
+                total = quantidade * 2.0;
+            }
+            else
+            {
+                total = quantidade * 1.5;
+            }
+
+            Console.WriteLine("Total: R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
         }
     }
 }
