f7afcd025cb95e4c145d90fa9cae4b682336e6bb
VitorDLeo <vitordileo003@gmail.com>
2022-05-23 23:36:53 -0300

    xxxxxxxxxxxxxxx

---
+++ Algoritmos/cap04/ex-resol-03/ex-resol-03/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ex_resol_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, n3, menor;
+
+            n1 = int.Parse(Console.ReadLine());
+            n2 = int.Parse(Console.ReadLine());
+            n3 = int.Parse(Console.ReadLine());
+
+            menor = n1;
+
+            if(n2 < n1 && n2 < n3)
+            {
+                menor = n2;
+            } else if(n3 < n1 && n3 < n2)
+            {
+                menor = n3;
+            }
+            Console.WriteLine("MENOR = " + menor);
+        }
+    }
+}
