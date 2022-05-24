18f1228ddb0991c661886da723457b1e443d3ddb
VitorDLeo <vitordileo003@gmail.com>
2022-05-23 23:57:19 -0300

    xxxxxxxxxxxxxxx

---
+++ Algoritmos/cap04/URI-ex-1038/URI-ex-1038/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace URI_ex_1038
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cod;
+            double qtd, total;
+            string[] vet;
+
+            vet = Console.ReadLine().Split(' ');
+            cod = int.Parse(vet)
+
+            switch()
+            
+        }
+    }
+}
