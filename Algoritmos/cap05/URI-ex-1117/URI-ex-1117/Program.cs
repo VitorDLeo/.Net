02624920efe4b4f5966324d6cc93e79ae9ae309f
VitorDLeo <vitordileo003@gmail.com>
2022-05-25 16:16:40 -0300

    Media com Estrutura de repeticao

---
+++ Algoritmos/cap05/URI-ex-1117/URI-ex-1117/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace URI_ex_1117
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double media, nota, soma;
+            int cont;
+
+            soma = 0;
+            cont = 0;
+            while (cont != 2)
+            {
+                nota = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                if (nota >= 0.0 && nota <= 10.0)
+                {
+                    soma += nota;
+                    cont += 1;
+                } else
+                {
+                    Console.WriteLine("nota invalida");
+                }
+            }
+
+            media = soma / cont;
+            Console.WriteLine("media = " + media.ToString("F2"), CultureInfo.InvariantCulture);
+        }
+    }
+}
