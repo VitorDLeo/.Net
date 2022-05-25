c42dafce585193b1fd136300577404da7364d432
VitorDLeo <vitordileo003@gmail.com>
2022-05-25 17:04:50 -0300

    Soma de idades e media com Loop WHILE

---
+++ Algoritmos/cap05/URI-ex-1154/URI-ex-1154/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace URI_ex_1154
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double idade, soma, media;
+            int count;
+
+            soma = 0;
+            media = 0;
+            count = 0;
+
+            idade = int.Parse(Console.ReadLine());
+
+            while(idade > 0)
+            {
+                soma += idade;
+                count += 1;
+                idade = int.Parse(Console.ReadLine());
+            }
+
+            media = (double) soma / count;
+
+            Console.WriteLine(media.ToString("F2"), CultureInfo.InvariantCulture);
+        }
+    }
+}
