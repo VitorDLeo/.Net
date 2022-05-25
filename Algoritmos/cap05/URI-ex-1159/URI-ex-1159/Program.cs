ebe69db831f0bb5c7be80a879efc251386b3b2bd
VitorDLeo <vitordileo003@gmail.com>
2022-05-25 17:11:42 -0300

    Soma de Pares com WHILE

---
+++ Algoritmos/cap05/URI-ex-1159/URI-ex-1159/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace URI_ex_1159
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+
+            while(x != 0)
+            {
+                if (x % 2 != 0)
+                {
+                    x = x + 1;
+                }
+
+                int soma = x + x + 2 + x + 4 + x + 6 + x + 8;
+
+                Console.WriteLine(soma);
+
+
+            }
+        }
+    }
+}
